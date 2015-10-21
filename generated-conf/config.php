383952c8ffe27432ec6bd16a94a84e45d7adec0a
arneau <arneau@onedayonly.co.za>
2015-10-21 07:40:56 +0200

    Miscellaneous Update

--- generated-conf/config.php
+++ generated-conf/config.php
@@ -3,19 +3,19 @@ $serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
 $serviceContainer->checkVersion('2.0.0-dev');
 $serviceContainer->setAdapterClass('default', 'mysql');
 $manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
-$manager->setConfiguration(array (
-  'dsn' => 'mysql:host=127.0.0.1;dbname=gw',
-  'user' => 'root',
-  'password' => '',
-  'settings' =>
-  array (
-    'charset' => 'utf8',
-    'queries' =>
-    array (
-    ),
-  ),
-  'classname' => '\\Propel\\Runtime\\Connection\\ConnectionWrapper',
-));
+$manager->setConfiguration([
+	'dsn'       => 'mysql:host=127.0.0.1;dbname=gw',
+	'user'      => 'root',
+	'password'  => '',
+	'settings'  =>
+		[
+			'charset' => 'utf8',
+			'queries' =>
+				[
+				],
+		],
+	'classname' => '\\Propel\\Runtime\\Connection\\ConnectionWrapper',
+]);
 $manager->setName('default');
 $serviceContainer->setConnectionManager('default', $manager);
 $serviceContainer->setDefaultDatasource('default');
